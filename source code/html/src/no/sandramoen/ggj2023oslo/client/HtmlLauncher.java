2537d8eb434e3054df999be4d685fff4151de98d
Sandra Moen <sandramoen01@gmail.com>
2023-02-05 10:43:54 +0100

    added preloadlogo.png

--- source code/html/src/no/sandramoen/ggj2023oslo/client/HtmlLauncher.java
+++ source code/html/src/no/sandramoen/ggj2023oslo/client/HtmlLauncher.java
@@ -3,21 +3,42 @@ package no.sandramoen.ggj2023oslo.client;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.backends.gwt.GwtApplication;
 import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.badlogic.gdx.backends.gwt.preloader.Preloader;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.dom.client.Style;
+import com.google.gwt.user.client.ui.Panel;
+
 import no.sandramoen.ggj2023oslo.MyGdxGame;
 
 public class HtmlLauncher extends GwtApplication {
 
-        @Override
-        public GwtApplicationConfiguration getConfig () {
-                // Resizable application, uses available space in browser
-                return new GwtApplicationConfiguration(true);
+    @Override
+    public GwtApplicationConfiguration getConfig() {
+        // Resizable application, uses available space in browser
+        GwtApplicationConfiguration config = new GwtApplicationConfiguration(true);
+        config.padHorizontal = 0;
+        config.padVertical = 0;
+        return new GwtApplicationConfiguration(true);
 
-                // Fixed size application:
-                // return new GwtApplicationConfiguration(688, 387);
-        }
+        // Fixed size application:
+        // return new GwtApplicationConfiguration(688, 387);
+    }
 
-        @Override
-        public ApplicationListener createApplicationListener () {
-                return new MyGdxGame();
-        }
+    @Override
+    public ApplicationListener createApplicationListener() {
+        return new MyGdxGame();
+    }
+
+    @Override
+    public Preloader.PreloaderCallback getPreloaderCallback() {
+        return createPreloaderPanel(GWT.getHostPageBaseURL() + "preloadlogo.png");
+    }
+
+    @Override
+    protected void adjustMeterPanel(Panel meterPanel, Style meterStyle) {
+        meterPanel.setStyleName("gdx-meter");
+        meterPanel.addStyleName("nostripes");
+        meterStyle.setProperty("backgroundColor", "#ffffff");
+        meterStyle.setProperty("backgroundImage", "none");
+    }
 }
